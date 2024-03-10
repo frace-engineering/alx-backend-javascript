e3810385408c543f2f41f6ed33efae7e22578885
Engr Ugwu <fugwungene@gmail.com>
2024-03-10 11:06:15 +0000

    Add advanced tasks.

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,14 @@
+export default function createIteratorObject(report) {
+  return {
+    * [Symbol.iterator]() {
+      for (const department in report.allEmployees) {
+        if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+          const employees = report.allEmployees[department];
+          for (const employee of employees) {
+            yield employee;
+          }
+        }
+      }
+    },
+  };
+}
