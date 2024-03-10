7ff1a25eda029224f610f87f52c58bbcd342d2f1
Engr Ugwu <fugwungene@gmail.com>
2024-03-10 15:35:58 +0000

    Add more tasks.
    Add task 4, 5, and 6.

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status, value: status === 'fulfilled' ? value : `Error: ${reason.messgae}`,
+    })));
+}
