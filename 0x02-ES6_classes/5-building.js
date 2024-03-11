579bcaf4890d7ddf7e5cbc30e6d966aa1db59d0f
Engr Ugwu <fugwungene@gmail.com>
2024-03-11 18:59:06 +0000

    Fix typo in code.

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = typeof sqft === 'number' ? sqft : 0;
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw ('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(value) {
-    this._sqft = value;
-  }
-
-  get evacuationWarningMessage() {
-    return 'Class extending Building must override evacuationWarningMessage';
-    }	
 }
--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    { id: 1, furstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, furstName: 'James', location: 'Columbia' },
-    { id: 5, furstName: 'Serena', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
