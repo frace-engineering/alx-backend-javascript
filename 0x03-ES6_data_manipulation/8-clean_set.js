b0ef2b4c6f7987e29df37501eddfcd64f545a7f8
Engr Ugwu <fugwungene@gmail.com>
2024-03-11 23:20:19 +0000

    Add 3 more tasks.
    Add task 7, 8 and task 9.

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  const array = Array.from(set);
+  const newArray = array.filter((value) => value.startsWith(startString));
+  return newArray.map((value) => value.slice(startString.length)).join('-');
+}
